65e844a808a33a3c6e6bc9745b5a9fee64c8a32d
Khunt Jenish <jenishkhunt27@gmail.com>
2022-12-24 18:48:56 +0530

    half work done

--- lib/screen/homepage/homecontroller.dart
+++ lib/screen/homepage/homecontroller.dart
@@ -1,5 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:get/get.dart';
 
 class HomepageController extends GetxController {
   int abc = 0;
+  RxList<List<dynamic>> data = RxList<List<dynamic>>();
+  String? filePath;
+
+  void pickFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    // if no file is picked
+    if (result == null) return;
+    // we will log the name, size and path of the
+    // first picked file (if multiple are selected)
+    print(result.files.first.name);
+    filePath = result.files.first.path!;
+
+    final input = File(filePath!).openRead();
+    final fields = await input
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter())
+        .toList();
+    print(fields);
+
+    data.value = fields;
+  }
 }
--- lib/screen/homepage/homepage.dart
+++ lib/screen/homepage/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class Homepage extends StatelessWidget {
   static const routeName = '/homepage';
@@ -23,20 +24,80 @@ class Homepage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Container(
-          child: Column(
-        children: [],
-      )),
-      floatingActionButton: SizedBox(
-        height: 100,
-        width: 100,
-        child: FloatingActionButton(
-          child: Icon(Icons.add), //child widget inside this button
-          onPressed: () {
-            print("Button is pressed.");
-            //task to execute when this button is pressed
-          },
+      body: Obx(
+        () => Container(
+          child: ListView.builder(
+            itemCount: _homepageController.data.length,
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemBuilder: (_, index) {
+              DateTime tempDate = index != 0
+                  ? DateFormat("dd.MM.yyyy")
+                      .parse(_homepageController.data[index][1].toString())
+                  : DateTime.now();
+              // tempDate = DateFormat("dd-MM-yyyy").format(tempDate);
+              print(DateFormat("dd-MM-yyyy").format(tempDate));
+              return Card(
+                margin: const EdgeInsets.all(3),
+                color: index == 0 ? Colors.amber : Colors.white,
+                child: ListTile(
+                  leading: Text(
+                    _homepageController.data[index][0].toString(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: index == 0 ? 18 : 15,
+                        fontWeight:
+                            index == 0 ? FontWeight.bold : FontWeight.normal,
+                        color: index == 0 ? Colors.red : Colors.black),
+                  ),
+                  title: Text(
+                    _homepageController.data[index][3],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: index == 0 ? 18 : 15,
+                        fontWeight:
+                            index == 0 ? FontWeight.bold : FontWeight.normal,
+                        color: index == 0 ? Colors.red : Colors.black),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        _homepageController.data[index][9].toString(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: index == 0 ? 18 : 15,
+                            fontWeight: index == 0
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                            color: index == 0 ? Colors.red : Colors.black),
+                      ),
+                      const SizedBox(width: 50),
+                      Text(
+                        DateFormat("dd-MM-yyyy").format(tempDate).toString(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: index == 0 ? 18 : 15,
+                            fontWeight: index == 0
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                            color: index == 0 ? Colors.red : Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add), //child widget inside this button
+        onPressed: () {
+          print("Button is pressed.");
+          _homepageController.pickFile();
+          //task to execute when this button is pressed
+        },
       ),
       drawer: drawer(),
     );
