185cad3bfc0f0fffe29e4800cd9b00b14f2ca9b1
Khunt Jenish <jenishkhunt27@gmail.com>
2023-01-05 18:24:20 +0530

    office work 5-1-22

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,9 +1,14 @@
+import 'package:csvapp/screen/homepage/generatedReport.dart';
+import 'package:csvapp/screen/homepage/homecontroller.dart';
+import 'package:csvapp/screen/homepage/ledgerReport.dart';
+import 'package:csvapp/screen/homepage/pendingReport.dart';
 import 'package:csvapp/utils/drawer.dart';
-
+import 'package:csvapp/utils/helper_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'screen/homepage/homecontroller.dart';
+import 'theam/theam_constants.dart';
+import 'utils/userBottomsheet.dart';
 
 class Mainpage extends StatelessWidget {
   static const routeName = '/mainPage';
@@ -26,7 +31,61 @@ class Mainpage extends StatelessWidget {
         centerTitle: true,
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(Get.width * 0.03),
-          child: BottomAppBar(),
+          child: Container(
+            width: Get.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ReportLabel(
+                  index: 1,
+                  text: 'Import Report',
+                  icon: Icon(Icons.insert_chart),
+                  onTap: () {
+                    _homepageController.isSelectedReport.value = 1;
+                    Get.toNamed(PendingReport.routeName);
+                  },
+                ),
+                ReportLabel(
+                  index: 2,
+                  text: 'Generated Report',
+                  icon: Icon(Icons.auto_graph),
+                  onTap: () {
+                    _homepageController.isSelectedReport.value = 2;
+                    Get.toNamed(GeneratedReport.routeName);
+                  },
+                ),
+                ReportLabel(
+                  index: 3,
+                  text: 'Party Payment',
+                  icon: Icon(Icons.payment),
+                  onTap: () {
+                    _homepageController.isSelectedReport.value = 3;
+                    // Get.toNamed(GeneratedReport.routeName);
+                  },
+                ),
+                ReportLabel(
+                  index: 4,
+                  text: 'Party Ledger',
+                  icon: Icon(Icons.receipt_long),
+                  onTap: () {
+                    _homepageController.isSelectedReport.value = 4;
+                    Get.toNamed(LedgerReport.routeName);
+                  },
+                ),
+
+                // ListTile(
+                //   leading: Icon(Icons.receipt_long),
+                //   title: Text(
+                //     'Party Ledger',
+                //     style: _textTheme.bodyText1?.copyWith(
+                //       color: Colors.white,
+                //       fontSize: Get.height * 0.02,
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
         ),
       ),
       drawer: drawer(),
